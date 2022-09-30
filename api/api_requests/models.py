c01971b70f306f37c7da3c29829b6eaceb9c2db7
hunter <hunter@hunter.hunter.xyx>
2022-09-30 11:17:11 -0300

    add views and branch in the project

--- api/api_requests/models.py
+++ api/api_requests/models.py
@@ -5,21 +5,25 @@ from django.db import models
 # Create your models here.
 class DataFly(models.Model):
     fecha_i = models.CharField(max_length=150)
-    vlo_i = models.IntegerField(max_length=150)
+    vlo_i = models.IntegerField()
     ori_i = models.CharField(max_length=150)
     des_i = models.CharField(max_length=150)
     emp_i = models.CharField(max_length=150)
-    fecha_i = models.CharField(max_length=150)
+    fecha_o = models.CharField(max_length=150)
     vlo_o = models.CharField(max_length=150)
     ori_o = models.CharField(max_length=150)
     des_o = models.CharField(max_length=150)
     emp_o = models.CharField(max_length=150)
-    day_month = models.IntegerField(max_length=150)
-    month_fly = models.IntegerField(max_length=150)
-    year_fly = models.IntegerField(max_length=150)
-    day_fly = models.IntegerField(max_length=150)
+    day_month = models.IntegerField()
+    month_fly = models.IntegerField()
+    year_fly = models.IntegerField()
+    day_fly = models.IntegerField()
     type_fly = models.CharField(max_length=2)
     enterprice = models.CharField(max_length=200)
     siglaori = models.CharField(max_length=200)
     siglades = models.CharField(max_length=200)
 
+
+
+
+# (fecha_i = "05092022", vlo_i=1, ori_i="buenos aires", des_i="cordoba", emp_i="aerolineas", fecha_o="05092022", vlo_o="n2m31nzx3", ori_o="bsas", des_o="co", emp_o="al", day_month=5, month_fly=9, year_fly=2022, day_fly=2, type_fly="N", enterprice="aerolineas argentina", siglaori="caba", siglades="jesus maria")
--- api/api_requests/views.py
+++ api/api_requests/views.py
@@ -1,9 +1,50 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
 import json
 from django.http import JsonResponse
 import requests
+from django.views.decorators.csrf import csrf_exempt
+from .models import DataFly
 # Create your views here.
 
 def getData(requests):
-    data = {'a':1, 'b':2, 'c':3}
-    return JsonResponse(data)
+    if requests.method == "GET":
+        data = {'a':1, 'b':2, 'c':3}
+        dataSqlite = DataFly.objects.all().values()
+        dataJson = {'data': list(dataSqlite)}
+        print(dataJson)
+        return JsonResponse(dataJson)
+
+@csrf_exempt
+def putData(requests):
+    # nombre de los campos de la tabla.
+    column_names = [
+            "fecha_i",
+            "vlo_i",
+            "ori_i",
+            "des_i",
+            "emp_i",
+            "fecha_o",
+            "vlo_o",
+            "ori_o",
+            "des_o",
+            "emp_o",
+            "day_month",
+            "month_fly",
+            "year_fly",
+            "day_fly",
+            "type_fly",
+            "enterprice",
+            "siglaori",
+            "siglades",
+    ]
+    if requests.method == "POST":
+        print(json.loads(requests.body)["data"])
+        for item in json.loads(requests.body)["data"]:
+            print(item)
+            for name in item:
+                print(name)
+                if name not in column_names:
+                    return JsonResponse({'response':'Error in format object, veried your data.'})
+        return JsonResponse({'response':'Data load'})
